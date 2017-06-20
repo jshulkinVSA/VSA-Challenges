ddae5b42ef5b8229274021e70091b2d027163774
jshulkinVSA <jshulkin12@gmail.com>
2017-06-20 15:26:16 -0500

    Add files via upload

---
+++ proj04.py
@@ -0,0 +1,12 @@
+# Name:
+# Date:
+
+"""
+proj04
+
+Asks the user for a string and prints out whether or not the string is a palindrome.
+
+"""
+word_input= raw_input("Please Enter A Word")
+
+word_input_list=
