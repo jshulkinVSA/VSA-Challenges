e1c131198822976b10af3033097e3bba184e6116
jshulkinVSA <jshulkin12@gmail.com>
2017-06-28 10:32:39 -0500

    Add files via upload

--- RSS_Feed.py
+++ RSS_Feed.py
@@ -11,7 +11,6 @@ from news_gui import Popup
 #
 # proj08: RSS Feed Filter
 
-
 #======================
 # Code for retrieving and parsing
 # Google and Yahoo News feeds
@@ -42,13 +41,11 @@ def process(url):
 #======================
 # Part 1
 # Data structure design
-
 #======================
 
 # Problem 1
 
-#TODO: NewsStory
-
+# TODO: NewsStory
 
 class NewsStory(object):
     def __init__(self,guid,title,subject,summary,link):
@@ -77,10 +74,6 @@ class NewsStory(object):
 
     def get_link(self):
         return self.link
-
-
-
-
 #======================
 # Part 2
 # Triggers
@@ -96,43 +89,95 @@ class Trigger(object):
 
 # Whole Word Triggers
 # Problems 2-5
+import string
+class WordTrigger(Trigger):
+    def __init__(self, word):
+        self.word=word.lower()
+    def is_word_in(self, story):
+        story=story.lower()
+        for item in string.punctuation:
+            story=story.replace(item, ' ')
+        story=story.split(' ')
+        if self.word in story:
+            print True
+            return True
+        else:
+            print False
+            return False
+
+
+
 
 # TODO: WordTrigger
-class WordTrigger(Trigger):
-    def __init__ (self,word):
-        def is_word_in(self, story):
-            import string
-                word=word.lower()
-                word=word.upper()
-        print string.punctuation
-                    s.replace('old punctuation','new punctuaion')
-                    s .split('delim')
-                    s.lower(punctuation), s.upper(punctuation)
-            if is_word_in in whole word:
-                return True
-            else:
-                return False
-
-
-
 
 # TODO: TitleTrigger
+class TitleTrigger(WordTrigger):
+    def evaluate (self,story):
+        story.get_title()
+        title=story.get_title()
+        return self.is_word_in(title)
 # TODO: SubjectTrigger
+class SubjectTrigger(WordTrigger):
+    def evaluate (self,story):
+        story.get_subject()
+        subject=story.get_subject()
+        return self.is_word_in(subject)
 # TODO: SummaryTrigger
-
+class SummaryTrigger(WordTrigger):
+    def evaluate (self,story):
+        story.get_summary()
+        summary=story.get_summary()
+        return self.is_word_in(summary)
 
 # Composite Triggers
 # Problems 6-8
 
 # TODO: NotTrigger
-# TODO: AndTrigger
-# TODO: OrTrigger
+class NotTrigger(Trigger):
+    def __init__(self,Trigger):
+        self.Trigger=Trigger
+    def evaluate (self,story):
 
+        return not self.Trigger.evaluate(story)
+# TODO: AndTrigger
+class AndTrigger(Trigger):
+    def __init__(self,Trigger1, Trigger2):
+        self.Trigger1=Trigger1
+        self.Trigger2=Trigger2
+
+    def evaluate(self, story):
+        if self.Trigger1.evaluate(story)==True and self.Trigger2.evaluate(story)==True:
+            return self.Trigger1
+            return self.Trigger2
+        else:
+            return False
+ # #TODO: OrTrigger
+class OrTrigger(Trigger):
+    def __init__(self,Trigger1,Trigger2):
+        self.Trigger1=Trigger1
+        self.Trigger2=Trigger2
+
+    def evaluate(self,story):
+        if self.Trigger1.evaluate(story)==True or self.Trigger2.evaluate(story)==True:
+            return self.Trigger1 or self.Trigger2
+        else:
+            return False
 
 # Phrase Trigger
 # Question 9
 
 # TODO: PhraseTrigger
+class PhraseTrigger(Trigger):
+    import string
+    def __init__(self,Phrase,):
+        self.Phrase=Phrase
+    def evaluate(self,story):
+        if self.Phrase in story.get_title() or self.Phrase in story.get_summary() or self.Phrase in story.get_subject():
+            return True
+            print True
+        else:
+            return False
+            print False
 
 
 #======================
@@ -149,7 +194,14 @@ def filter_stories(stories, triggerlist):
     # TODO: Problem 10
     # This is a placeholder (we're just returning all the stories, with no filtering) 
     # Feel free to change this line!
-    return stories
+    Answerlist=[]
+    for story in stories:
+       for trigger in triggerlist:
+            if trigger.evaluate(story):
+                Answerlist.append(story)
+    return Answerlist
+
+
 
 #======================
 # Extensions: Part 4
